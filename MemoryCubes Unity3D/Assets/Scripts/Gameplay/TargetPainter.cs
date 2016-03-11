e077cd37e87693ad0b6952332a28ee800306cc27
Nils Rietveld <nils.rietveld@boostermedia.com>
2016-03-11 09:06:00 +0100

    Drawing target bars on start

--- MemoryCubes Unity3D/Assets/Scripts/Gameplay/TargetPainter.cs
+++ MemoryCubes Unity3D/Assets/Scripts/Gameplay/TargetPainter.cs
@@ -1,11 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetPainter : MonoBehaviour {
+    
+    private GameObject[,] targetBarSprites;
+    
+    private Vector2 targetBarArraySize = Vector2.zero;
+    
+    public GameObject targetSpritePrefab;
+    
+    public GameObject[] targetBarPlaceholders;
+    
+    public int targetBarLength = 25;
+    
+    public int direction;
 
 	// Use this for initialization
 	void Start() 
     {
-	   GameObject.Find("Game").GetComponent<TargetSelector>().NextTargetEvent += OnNextTargetEvent;
+        targetBarArraySize.x = targetBarPlaceholders.Length;
+        targetBarArraySize.y = targetBarLength;
+        
+        targetBarSprites = new GameObject[(int)targetBarArraySize.x, (int)targetBarArraySize.y];
+        
+        for (int x = 0; x < targetBarArraySize.x; x++)
+        {
+            for (int y = 0; y < targetBarArraySize.y; y++)
+            {
+                GameObject targetBarSprite = GameObject.Instantiate(targetSpritePrefab);
+                
+                targetBarSprite.transform.SetParent(this.transform, false);
+                
+                Image image = targetBarSprite.GetComponent<Image>();
+                
+                // Horizontal position is based on y and vertical position is based on placeholder position
+                Vector2 position = new Vector2(
+                    targetBarPlaceholders[x].transform.position.x + (image.rectTransform.sizeDelta.x * y) * direction,
+                    targetBarPlaceholders[x].transform.position.y
+                );
+                
+                targetBarSprite.transform.position = position;
+                
+                targetBarSprites[x, y] = targetBarSprite;
+            }
+        }
+        
+        for (int i = 0; i < targetBarPlaceholders.Length; i++)
+        {
+            Destroy(targetBarPlaceholders[i]);
+        }
+        
+	    GameObject.Find("Game").GetComponent<TargetSelector>().NextTargetEvent += OnNextTargetEvent;
 	}
 	
 	// Update is called once per frame
@@ -21,19 +66,7 @@ public class TargetPainter : MonoBehaviour {
     
     private void CreateTargetBar(NextTargetEventArgs e)
     {
-        // Create 2 dimensional array with target fracture sprites
-        // Make the placeholder sprites invisible
-        // Start positioning based on those placeholder sprites and move outwards on x
-        
+        // Set correct color based on target (image swap)
         // Add a timer and based on time, set sprites visible/invisible
-        
-        Sprite targetSprite = LoadSprite(e.targetColor);
-    }
-    
-    private Sprite LoadSprite(TargetColors targetColor)
-    {
-        Sprite sprite = (Sprite)Resources.Load(targetColor.ToString());
-        
-        return sprite;
     }
 }
