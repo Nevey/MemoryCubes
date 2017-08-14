78e0917a5e3da004f794ed0be10f5e9907756de9
Nils Rietveld <its.just.nils@gmail.com>
2017-08-14 21:59:45 +0200

    cleanup

--- MemoryCubes Unity3D/Assets/Scripts/Grid/GridCollector.cs
+++ MemoryCubes Unity3D/Assets/Scripts/Grid/GridCollector.cs
@@ -3,12 +3,8 @@ using System;
 
 public class GridCollector : MonoBehaviour
 {
-    //public event EventHandler<CollectEventArgs> CollectEvent;
-
     public static event Action CollectEvent;
-
-    // TODO: keep a list of collected cubes?
-
+    
 	public void OnCollectPressed()
     {        
         if (CollectEvent != null)
