e7270099f5a5a607b338f1089c4c0cadcf1f4722
Nils <its.just.nils@gmail.com>
2017-10-16 07:56:08 +0200

    [WIP] adding particle effect on tile destroy

---
+++ MemoryCubes Unity3D/Assets/Scripts/Effects/PlayParticles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticles : MonoBehaviour
+{
+	[SerializeField] private GameObject particlesObjectPrefab;
+
+	private GameObject particlesObject;
+
+	private void ShowParticles(Vector3 position)
+	{
+		particlesObject = Instantiate(particlesObjectPrefab);
+
+		particlesObject.transform.position = position;
+	}
+}
